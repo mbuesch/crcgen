eaa5d091b7c6b54565882eda7f511506e8a6b3de
Michael Buesch <m@bues.ch>
2021-07-13 21:06:10 +0200

    Update release script

--- maintenance/makerelease.sh
+++ maintenance/makerelease.sh
@@ -8,7 +8,7 @@ srcdir="$srcdir/.."
 die() { echo "$*"; exit 1; }
 
 # Import the makerelease.lib
-# http://bues.ch/gitweb?p=misc.git;a=blob_plain;f=makerelease.lib;hb=HEAD
+# https://bues.ch/cgit/misc.git/plain/makerelease.lib
 for path in $(echo "$PATH" | tr ':' ' '); do
 	[ -f "$MAKERELEASE_LIB" ] && break
 	MAKERELEASE_LIB="$path/makerelease.lib"
